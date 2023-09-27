54a17e5b4bb5367bd9d4242ca8af511b176776cd
sohyeonjung <jhyeon23@konkuk.ac.kr>
2023-09-27 09:36:59 +0900

    setting method, constructor

--- src/LockerManager.java
+++ src/LockerManager.java
@@ -1,6 +1,26 @@
 public class LockerManager {
 
+    Locker locker = new Locker();
 
+    //Constructor
+    public LockerManager() {
+
+    }
+    public LockerManager(String userId, String userPassword) {
+
+    }
+    
+    //예약 메소드
+    private void Booking(){
+        /**/
+
+    }
+
+    //예약 확정 메소드
+    private void ConfirmBooking(){
+        /**/
+
+    }
 
 
 }
