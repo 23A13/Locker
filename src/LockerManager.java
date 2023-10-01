b831d5e2d3194385b6cde0f8df47950da22edd56
sohyeonjung <jhyeon23@konkuk.ac.kr>
2023-10-01 21:25:27 +0900

    error fixing

--- src/LockerManager.java
+++ src/LockerManager.java
@@ -54,33 +54,36 @@ public class LockerManager {
         //처음 안내 출력
         locker.print(); //Locker class의 print 기능사용하여 사물함 프린트
 
+        String tariff = """
+                ——————요금표——————————————————————————\s
+                기본 4시간\s
+                S : 2000원 / M : 3000원 / L : 4000원\s
+                시간당 추가요금\s
+                S : 500원 / M : 800원 / L : 1000원\s
+                —————————————————————————————————————\s
+                                
+                ——물품보관함 사이즈 안내————————\s
+                S : 01~08번 보관함 (총 8개)\s
+                M : 09~12번 보관함 (총 4개)\s
+                L : 13~16번 보관함 (총 4개)\s
+                ————————————————————————————\s
+                                
+                이용하실 보관함의 번호를 입력하세요.\s
+                                
+                * 이전 메뉴로 돌아가려면 Q 또는 q를 입력하세요.\s
+                ————————————————————————————————————————\s
+                                
+                                
+                """;
+
         //입력
         int LockerNumber=0;
 
+        //흐름에 따라 flow 값 변경. 보관함 선택 (1) -> 사이즈 별 안내, 결제 확인 (2) -> 결제 (3)
+        int flow = 1;
+
         while(true){
-            System.out.println("——————요금표—————— ");
-            System.out.println("기본 4시간");
-            System.out.println("S:2000원/M:3000원/L:4000원");
-            System.out.println("시간당 추가요금");
-            System.out.println("S:500원/M:800원/L:1000원");
-            System.out.println("——————————————————\n\n");
-            System.out.println("——물품보관함 사이즈 안내——");
-            System.out.println("S : 01~08번 보관함 (총 8개) ");
-            System.out.println("M : 09~12번 보관함 (총 4개) ");
-            System.out.println("L : 13~16번 보관함 (총 4개) ");
-            System.out.println("——————————————————————\n\n");
-            System.out.println("———————————예약 서비스 이용 시 주의사항————————————");
-            System.out.println("* 예약 시 4시간 선결제 후 보관함에 도착하여 물품을 보관해주세요");
-            System.out.println("* 예약 후 2시간 내로 물품 보관이 완료되지 않을 경우, 예약이 자동 취소됩니다. (환불 불가)");
-            System.out.println("* 기본 4시간 초과 시 수거하는 시점까지 자동으로 시간 당 추가 요금이 부과됩니다. (추가요금은 요금표 참고) ");
-            System.out.println("* 추가 요금은 보관함 수거 시 부과됩니다. ");
-            System.out.println("* 결제 수단은 카드만 사용 가능합니다. ");
-            System.out.println("* 이용 중인 보관함은 예약이 불가합니다. ");
-            System.out.println("* 예약은 보관함 한 개만 가능합니다. ");
-            System.out.println("—————————————————————————————————————————————\n\n");
-            System.out.println("이용 하실 보관함의 번호를 입력하세요.\n");
-            System.out.println("* 이전 메뉴로 돌아가려면 Q 또는 q를 입력하세요.\n");
-            System.out.println("—————————————————————————————————————————————");
+            System.out.println(tariff);
 
             //입력
             System.out.print(">>");
@@ -106,6 +109,8 @@ public class LockerManager {
                 //예약 확인 처리 !!!!!!!1아직 상의 안 된 부분 바뀔 수도 있음
                 if(locker.BooingCheck(LockerNumber)) throw new IllegalAccessException();
 
+                //아무 문제 없다면 사이즈 별 안내 창으로 이동
+                flow = 2;
                 break;
 
             }catch(IllegalArgumentException e){ //나머지 입력 예외 처리
@@ -119,69 +124,55 @@ public class LockerManager {
 
         //사이즈 별 입력
 
-        String payment;
+        String payment = " ";
 
         //사이즈 별
-        while(true){
-            if(LockerNumber>0 && LockerNumber<9){
-                System.out.println("<"+LockerNumber+">"+"번 보관함을 선택하셨습니다.");
-                System.out.println("해당 보관함의 사이즈는 S이며 기본 4시간 2000원, 시간당 추가 요금은 500원입니다.\n");
-                System.out.print("결제 하시겠습니까? (Y/N) ");
+        if(flow==2){
+            while(true){
+                System.out.print(StoragePaymentPrompt(LockerNumber));
+                payment = String.valueOf(sc.next());
+                sc.nextLine();
+
+                try{
+                    if(!(payment.equals("Y")||payment.equals("N")||payment.equals("y")||payment.equals("n")))
+                        throw new IllegalArgumentException();
+
+                    //아무 문제 없다면 결제 창으로 이동
+                    flow = 3;
+                    break;
+                }catch(Exception e){
+                    System.out.println("올바른 입력이 아닙니다. 다시 한 번 입력해주세요.\n");
+                }
+
             }
-            else if(LockerNumber>8 && LockerNumber<13){
-                System.out.println("<"+LockerNumber+">"+"번 보관함을 선택하셨습니다.");
-                System.out.println("해당 보관함의 사이즈는 M이며 기본 4시간 3000원, 시간당 추가 요금은 800원입니다. \n");
-                System.out.print("결제 하시겠습니까? (Y/N) ");
+
+        }
+
+
+        if(flow==3){
+            if(payment.equals("Y")||payment.equals("y")){
+                System.out.println("\n보관함 예약이 완료되었습니다. ");
+                System.out.println("* 2시간 내로 물품 보관(예약 확정)을 완료해주시기 바랍니다.");
+                System.out.println("(물품 보관이 완료 되지 않을 경우 예약 자동 취소 및 환불 불가)");
+
+                System.out.println("");
+                //Date/Time prompt로 돌아가기
+                //!!!!!!!!!!UserManager의 날짜/시간 입력 부분으로 돌아감(여긴 UserManager에서 만들어야 함)
+                //UserManager.date();
+
+                //여기서 Locker 객체 불러와서 예약 여부 정해야 합니다. 혹은 여기서 파일 바로 저장.
+                // 근데 Locker 객체에서 저장하는게 더 좋을듯
+                locker.BooingFileInput(LockerNumber);
+
+                return;
             }
             else{
-                System.out.println("<"+LockerNumber+">"+"번 보관함을 선택하셨습니다.");
-                System.out.println("해당 보관함의 사이즈는 L이며 기본 4시간 4000원, 시간당 추가 요금은 1000원입니다. \n");
-                System.out.print("결제 하시겠습니까? (Y/N) ");
+                //!!!!!!!!!!!!!!아직 상의 안 된 부분 바뀔 수도 있음
+                System.out.println("\n\nmenu2.1로 돌아가기\n");
+                LockerManager.Menu();
+                return;
             }
-
-
-            //입력
-            payment = sc.next();
-
-            try{
-                if(!(payment.equals("Y")||payment.equals("N"))) throw new IllegalArgumentException();
-                break;
-            }catch(Exception e){
-                System.out.println("올바른 입력이 아닙니다. 다시 한 번 입력해주세요.\n");
-            }
-
         }
-        //
-        System.out.println(payment);
-
-        if(payment.equals("Y")){
-            System.out.println("\n보관함 예약이 완료되었습니다. ");
-            System.out.println("* 2시간 내로 물품 보관(예약 확정)을 완료해주시기 바랍니다.");
-            System.out.println("(물품 보관이 완료 되지 않을 경우 예약 자동 취소 및 환불 불가)");
-
-            System.out.println("");
-            //Date/Time prompt로 돌아가기
-            //!!!!!!!!!!UserManager의 날짜/시간 입력 부분으로 돌아감(여긴 UserManager에서 만들어야 함)
-            //UserManager.date();
-
-            //여기서 Locker 객체 불러와서 예약 여부 정해야 합니다. 혹은 여기서 파일 바로 저장.
-            // 근데 Locker 객체에서 저장하는게 더 좋을듯
-            locker.BooingFileInput(LockerNumber);
-
-            return;
-
-
-        }
-        else{
-            //!!!!!!!!!!!!!!아직 상의 안 된 부분 바뀔 수도 있음
-            System.out.println("\n\nmenu2.1로 돌아가기\n");
-            LockerManager.Menu();
-            return;
-        }
-
-
-
-
 
 
     }
@@ -191,6 +182,35 @@ public class LockerManager {
         /**/
 
     }
+
+    public static String StoragePaymentPrompt(int lockerNum) {
+        /*
+        storage_payment_prompt 를 출력하는 메서드
+         */
+        String strLockerNum = String.valueOf(lockerNum);
+        if(lockerNum < 10) {
+            strLockerNum = "0" + strLockerNum;
+        }
+        //보관함 크기에 따른 금액 차등
+        int payment = 0;
+        if(lockerNum <= 8)
+            payment = 2000;
+        else if (lockerNum <= 12) {
+            payment = 3000;
+        }
+        else
+            payment = 4000;
+
+        String str = "<" + strLockerNum + ">번 보관함 선택하셨습니다. \n" +
+                "기본(4시간) 금액 <" + payment + ">원 결제하시겠습니까? (y/n) \n" +
+                "\n" +
+                "* 초과 시간에 따른 요금은 수거 시 추가결제됩니다.\n" +
+                "—————————————————————————— \n" +
+                ">>";
+
+        return str;
+    }
+
 
 
 }
