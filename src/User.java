0bf2f8a1d73666da34620af160532b7c1441e040
jijang <jithogy@gmail.com>
2023-10-02 17:21:15 +0900

    User

--- src/User.java
+++ src/User.java
@@ -1,16 +1,16 @@
 public class User {
-    int member; //회원 비회원 여부
+    String member; //회원 비회원 여부-> 갑자기 저장할 필요가 있나 싶음..흠흠띠
     String memberID; //회원ID
-    int memberPW; //회원 비밀번호
-    int locknum; //사용중(예약중)인 보관함 번호
-    int lockPW; //보관함 비밀번호
+    String memberPW; //회원 비밀번호
+    String locknum; //사용중(예약중)인 보관함 번호
+    String lockPW; //보관함 비밀번호
 
     //constructor
     public User(){}
 
     //회원 생성자
-    public User(int member,String memberID,int memberPW,int locknum,int lockPW){
-        this.member=member;
+    public User(String memberID,String memberPW,String locknum,String lockPW){
+        this.member="1";
         this.memberID=memberID;
         this.memberPW=memberPW;
         this.locknum=locknum;
@@ -18,9 +18,21 @@ public class User {
     }
 
     //비회원 생성자
-    public User(int member,int locknum,int lockPW){
-        this.member=member;
+    public User(String locknum,String lockPW){
+        this.member="0";
+        this.memberID=null;
+        this.memberPW=null;
         this.locknum=locknum;
         this.lockPW=lockPW;
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        if(member.equals("1")){
+            return member+" "+memberID+" "+memberPW+" "+locknum+" "+lockPW+"\n";//회원
+        }else{
+            return member+" "+locknum+" "+lockPW+"\n";//비회원
+        }
     }
 }
