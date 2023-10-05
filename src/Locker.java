87174c8d3800961a42400e7e3d2228855e155a4d
sohyeonjung <jhyeon23@konkuk.ac.kr>
2023-10-05 11:54:14 +0900

    d

--- src/Locker.java
+++ src/Locker.java
@@ -1,4 +1,28 @@
-public class Locker {
+import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.locks.Lock;
+
+import static java.lang.Integer.*;
+import static java.lang.System.exit;
+
+public class LockerManager {
+
+    //로그인한 사용자 아이디
+    String loguser;
 
 
-}
+    static ArrayList<Locker> LockerList = new ArrayList<>(); //Locker정보 저장 구조
+    Map<String, User> mem = new HashMap<>(); //회원 정보 저장 구조
+    Map<String, User> nonmem = new HashMap<>(); //비회원 정보 저장 구조
+
+    UserManager usermanager = new UserManager();
+
+    //constructor
+
+
+    //Constructor
+    public LockerManager(){
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,28 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
+public class Locker {
 
-        System.out.println("정상실행확인");
+    String locknum; //보관함 번호
+    String locksize; //보관함 크기(0/1/2-S/M/L)
+    String use; //사용여부(0/1/2-미사용/사용중/예약중)
+    String date; //날짜시각
+    String confirmbook; //예약확정 여부(0/1-미확/확정)
 
+    //constructor
+    public Locker() {
     }
+    public Locker(String locknum, String locksize,String date){
+        this.locknum=locknum;
+        this.locksize=locksize;
+        this.use="0";
+        this.date=date;
+        this.confirmbook="0";
+    }
+    public Locker(String locknum, String locksize,String use,String date,String confirmbook){
+        this.locknum=locknum;
+        this.locksize=locksize;
+        this.use=use;
+        this.date=date;
+        this.confirmbook=confirmbook;
+    }
+
+
 }
