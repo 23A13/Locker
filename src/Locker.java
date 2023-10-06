a42fcbf2f1e07546e85ccc2b558d83c6d34bde62
ksk0823 <machine0823@naver.com>
2023-10-07 04:06:27 +0900

    사용 중인 보관함 미출력 오류 해결

--- src/Locker.java
+++ src/Locker.java
@@ -1,5 +1,7 @@
-import java.util.Map;
-import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Locker {
 
@@ -91,24 +93,42 @@ public class Locker {
         System.out.println("|           |           |           |           |                  |                  |");
         System.out.println("---------------------------------------------------------------------------------------");
 
-        for (User user : memMap.values()) {
-            int locknum = Integer.parseInt(User.locknum);
-            if (locknum > 0) {
-                System.out.println("사용 중인 보관함 :" + locknum);
-            }
-            else {
-                System.out.println("사용 중인 보관함이 존재하지 않습니다.");
-            }    
-        }
+        String fileName = "../Locker/User.txt";
 
-        for (User user : nonmemMap.values()) {
-            int locknum = Integer.parseInt(User.locknum);
-            if (locknum > 0) {
-                System.out.println("사용 중인 보관함 :" + locknum);
-            }
-            else {
-                System.out.println("사용 중인 보관함이 존재하지 않습니다.");
-            }
+        try {
+
+            //수정
+            String filePath = "../Locker/User.txt";
+            FileReader fileReader = new FileReader(filePath);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            System.out.print("사용중인 보관함 번호:");
+
+
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] parts = line.split(" ");
+                if (parts[0].equals("1")) {
+                    if(!parts[3].equals("-"))
+                    {
+                        String lockerNumber = parts[3];
+                        System.out.println("사용중인 보관함 번호: " + lockerNumber);
+                    }
+                }
+                else if (parts[0].equals("0")) {
+                    String lockerNumber = parts[1];
+                    System.out.println("사용중인 보관함 번호: " + lockerNumber);
+                }
+            } bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("파일을 찾을 수 없습니다: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("파일을 읽는 동안 오류가 발생했습니다: " + e.getMessage());
+        } catch (Exception e) {
+            System.err.println("예외가 발생했습니다: " + e.getMessage());
         }
     }
 }
