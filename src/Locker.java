6a010d4dd17203c1b0f81821bb3a6ecf43357794
SongeunKim <ucs155@konkuk.ac.kr>
2023-10-09 00:07:37 +0900

    getter/setter 위치 변경

--- src/Locker.java
+++ src/Locker.java
@@ -9,6 +9,25 @@ public class Locker {
     String locksize; //보관함 크기(0/1/2-S/M/L)
     String use; //사용여부(0/1/2-미사용/사용중/예약중)
     String date; //날짜시각
+    String confirmbook; //예약확정 여부(0/1-미확/확정)
+
+    //constructor
+    public Locker() {
+    }
+    public Locker(String locknum, String locksize,String date){
+        this.locknum=locknum;
+        this.locksize=locksize;
+        this.use="0";
+        this.date=date;
+        this.confirmbook="0";
+    }
+    public Locker(String locknum, String locksize,String use,String date,String confirmbook){
+        this.locknum=locknum;
+        this.locksize=locksize;
+        this.use=use;
+        this.date=date;
+        this.confirmbook=confirmbook;
+    }
 
     public String getLocknum() {
         return locknum;
@@ -48,26 +67,6 @@ public class Locker {
 
     public void setConfirmbook(String confirmbook) {
         this.confirmbook = confirmbook;
-    }
-
-    String confirmbook; //예약확정 여부(0/1-미확/확정)
-
-    //constructor
-    public Locker() {
-    }
-    public Locker(String locknum, String locksize,String date){
-        this.locknum=locknum;
-        this.locksize=locksize;
-        this.use="0";
-        this.date=date;
-        this.confirmbook="0";
-    }
-    public Locker(String locknum, String locksize,String use,String date,String confirmbook){
-        this.locknum=locknum;
-        this.locksize=locksize;
-        this.use=use;
-        this.date=date;
-        this.confirmbook=confirmbook;
     }
 
     public static void print(){
