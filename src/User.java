f982d132614591aad57640a7c5cd016d5ab34820
jijang <jithogy@gmail.com>
2023-10-29 00:35:59 +0900

    User클래스 수정

--- src/User.java
+++ src/User.java
@@ -4,7 +4,7 @@ public class User {
     String memberPW; //회원 비밀번호
     String locknum; //사용중(예약중)인 보관함 번호
     String lockPW; //보관함 비밀번호
-    String cannotUntil; //회원 예약 패널티 날짜
+    String cannotUntil="-"; //회원 예약 패널티 날짜
 
     //constructor
     public User() {
@@ -17,6 +17,15 @@ public class User {
         this.memberPW = memberPW;
         this.locknum = locknum;
         this.lockPW = lockPW;
+    }
+
+    public User(String memberID, String memberPW, String locknum, String lockPW,String cannotUntil) {
+        this.member = "1";
+        this.memberID = memberID;
+        this.memberPW = memberPW;
+        this.locknum = locknum;
+        this.lockPW = lockPW;
+        this.cannotUntil=cannotUntil;
     }
 
     //비회원 생성자
@@ -69,12 +78,15 @@ public class User {
         this.lockPW = lockPW;
     }
 
+    public String getCannotUntil(){return cannotUntil;}
+    public void setCannotUntil(String cannotUntil){this.cannotUntil=cannotUntil;}
+
 
     @Override
     public String toString() {
         // TODO Auto-generated method stub
         if(member.equals("1")){
-            return member+" "+memberID+" "+memberPW+" "+locknum+" "+lockPW+"\n";//회원
+            return member+" "+memberID+" "+memberPW+" "+locknum+" "+lockPW+" "+cannotUntil+"\n";//회원
         }else{
             return member+" "+locknum+" "+lockPW+"\n";//비회원
         }
