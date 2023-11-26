1179812efa4838154811ae09d34dd02e3f94814a
YunbinGil <bq0403@konkuk.ac.kr>
2023-11-26 23:01:36 +0900

    강제수거 메소드

--- src/AdminManager.java
+++ src/AdminManager.java
@@ -87,6 +87,155 @@ public class AdminManager {
 
     }
 
+    public void force_pickup() {
+        String noticeFp = """
+                --------------------------------------------------------------
+                물품을 강제 수거할 보관함 번호를 입력해주세요.
+                                
+                * 이전 메뉴로 돌아가려면 Q 또는 q를 입력하세요.
+                --------------------------------------------------------------
+                """;
+
+        int LockerNumber = 0;
+        Locker targetLocker = null;
+        int target = 0;
+        while (true) {
+            //noticeFp("강제수거 프롬프트") 출력
+            printAdminLocker();
+            System.out.print(noticeFp);
+
+            //보관함 번호 입력받기
+            System.out.print(">>");
+
+
+            try {
+                String LockerNum = String.valueOf(sc.next());
+                sc.nextLine();
+
+                //Q,q 처리
+                if (Objects.equals(LockerNum, "Q") || Objects.equals(LockerNum, "q")) {
+                    count++; //@지은 이거 무한안걸리게 하는거였는데 어떤 원리인지 설명듣고 수정해야할듯
+                    menu();
+                    return;
+                }
+
+                //형식 예외 처리(00, 01, 02 등으로 입력해야함)
+                if (LockerNum.length() != 2)
+                    throw new IllegalArgumentException();
+
+                //범위 예외 처리(01~99)
+                LockerNumber = Integer.parseInt(LockerNum);
+                if (LockerNumber < 1 || LockerNumber > 99)
+                    throw new IllegalArgumentException();
+
+                //존재하지 않는 보관함의 번호를 입력한 경우 처리]
+                //일단 for문으로 찾고
+                int i = 0;
+                targetLocker = null;
+                for (Locker lc : l.LockerList) {
+                    if (Integer.parseInt(lc.locknum) == LockerNumber) {
+                        targetLocker = lc;
+                        target = i;
+                        break;
+                    }
+                    i++;
+                }
+                if (targetLocker == null) {
+                    System.out.println("해당 번호의 보관함이 존재하지 않습니다.\n");
+                    throw new IllegalAccessException();
+                }
+
+                //사용 중이지 않은 보관함의 번호를 입력한 경우
+                if (targetLocker.use.equals("0")) {
+                    System.out.println("보관 중인 물품이 없습니다. \n");
+                    throw new IllegalAccessException();
+                }
+
+                //강제수거가 가능하지 않은 보관함을 선택했을 때 처리
+                for (Locker lc : l.LockerList) {
+                    if (Integer.parseInt(lc.locknum) == LockerNumber) {
+                        if (lc.iscanFp == false) {
+                            System.out.println("강제수거를 할 수 없는 보관함입니다. 강제수거 가능여부를 확인해주세요.\n");
+                            throw new IllegalAccessException();
+                        }
+                    }
+                }
+                break;
+            } catch (IllegalArgumentException e) { //나머지 입력 예외 처리
+                System.out.println("올바른 입력이 아닙니다. 다시 한 번 입력해주세요.\n");
+
+            } catch (IllegalAccessException e) {
+
+            }
+        }
+
+        //정상 결과
+        System.out.println();
+        String FpCheckNotice = targetLocker.locknum + "번 보관함을 선택하셨습니다.\n";
+        FpCheckNotice += targetLocker.locknum + "번 / " + targetLocker.date + " / " + (int) (Math.ceil((double) targetLocker.timediffMinutes / 60)) + "시간 / ";
+        if (targetLocker.iscanFp == true)
+            FpCheckNotice += "강제수거 가능";
+        else
+            FpCheckNotice += "강제수거 불가능";
+        FpCheckNotice += """
+                \n\n강제수거 하시려면 Y또는 y를 입력해주세요.
+                ------------------------------------------             
+                >> """;
+
+
+        System.out.print(FpCheckNotice);
+        String yn = String.valueOf(sc.next());
+        sc.nextLine();
+
+        if (!(Objects.equals(yn, "Y") || Objects.equals(yn, "y"))) {
+            System.out.println("강제수거를 취소하셨습니다. \n");
+            return; //menu3으로 돌아감
+        } else { //Y나 y를 입력한경우
+            //mem과 nonmem에서 해당 보관함 찾기 (회원<User> 정보 저장구조)
+            String targetKey = null;
+            boolean isMemLocker = false;
+
+            for (String id : u.memMap.keySet()) {
+                if (!u.memMap.get(id).locknum.equals("-")) {
+                    if (LockerNumber == Integer.parseInt(u.memMap.get(id).locknum)) {
+                        targetKey = id;
+                        break;
+                    }
+                }
+            }
+            if (targetKey != null) {
+                isMemLocker = true;
+            } else {
+                isMemLocker = false;
+                for (String lnum : u.nonmemMap.keySet()) {
+                    if (Integer.parseInt(u.nonmemMap.get(lnum).locknum) == LockerNumber) {
+                        targetKey = lnum;
+                        break;
+                    }
+                }
+            }
+
+            //강제수거 완료한 보관함 정보 삭제 & File Write
+            l.LockerList.get(target).use = "0";
+            l.LockerList.get(target).date = "-";
+            l.LockerList.get(target).confirmbook = "0";
+            l.LockerList.get(target).iscanFp = false;
+            l.LockerList.get(target).timediffMinutes = 0;
+
+            if (isMemLocker) {
+                u.memMap.get(targetKey).locknum = "-";
+                u.memMap.get(targetKey).lockPW = "-";
+            } else {
+                u.nonmemMap.remove(targetKey);
+            }
+
+            System.out.println("물품 강제 수거가 완료되었습니다. \n이용해주셔서 감사합니다.\n");
+
+            ExitWrite();
+
+
+        }
+    }
     public void timediffUpdate(Locker lc){
         Date currentTime = l.StringToDate(Main.currentTimeString);
         Date startTime = l.StringToDate(lc.date);
